5267731a31f652fc94d64e3d3da195b645eb626a
rymercyble <rymercyble@gmail.com>
2024-03-01 05:15:14 +0100

    project setup - breeze vue/inertia/tailwind, larastan, debugbar, ide helper, readme

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/ts/app.ts',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: [{ find: '@', replacement: path.resolve(__dirname, 'resources/ts') }],
+    },
 });
